13ebbc0d6ba5ad84a2005539ad134df42d3027a4
Federico Galatolo <galatolo.federico@gmail.com>
2021-04-29 16:14:19 +0200

    cleaner experiments script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+generations=200
+
 rm -rf results/*
 
-./run.sh results/toy_seq --experiment SequencePrototypeExperiment --generations 100
-./run.sh results/toy_th --experiment ThresholdPrototypeExperiment --generations 100
-./run.sh results/casestudy_th --experiment ThresholdCaseStudyExperiment --generations 100
+./run.sh results/toy_seq --experiment SequencePrototypeExperiment --generations $generations
+./run.sh results/toy_th --experiment ThresholdPrototypeExperiment --generations $generations
+./run.sh results/casestudy_th --experiment ThresholdCaseStudyExperiment --generations $generations
 
 python static_results.py --experiment ThresholdPrototypeExperiment --save results/static_toy_th
 python static_results.py --experiment ThresholdCaseStudyExperiment --save results/static_casestudy_th
